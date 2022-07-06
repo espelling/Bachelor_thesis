876ac3cc211e45982814f1cbd032eac33be0d53c
pulpy1983 <e.spelling@gmx.de>
2022-07-06 14:37:33 +0200

    update files

--- data extraction/download_timeSeries_Cases_JohnHopkins.py
+++ data extraction/download_timeSeries_Cases_JohnHopkins.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import wget
 
-
-# url of the raw csv dataset
+# Der Quellcode bezogen aus der Quelle: https://towardsdatascience.com/covid-19-data-processing-58aaa3663f6
+# bestätigte Corona-Fälle + Todes-Fälle + Genesenen-Fälle: Download der Daten (JHU) -> Url der Rohdaten im CSV-Format
 urls = [
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
--- data extraction/download_timeSeries_Doses_JohnHopkins.py
+++ data extraction/download_timeSeries_Doses_JohnHopkins.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import wget
 
-
-# url of the raw csv dataset
+# Der Quellcode bezogen aus der Quelle: https://towardsdatascience.com/covid-19-data-processing-58aaa3663f6
+# verabreichte Impfdosen: Download der Daten (JHU) -> Url der Rohdaten im CSV-Format
 urls = [
     'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_doses_admin_global.csv',
     'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv',
